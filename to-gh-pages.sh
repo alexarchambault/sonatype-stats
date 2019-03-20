d8106e68add91d92d2bc4d147268ed16001a7e85
Alexandre Archambault <alexandre.archambault@gmail.com>
2019-03-20 15:17:55 +0100

    Add to-gh-pages scripts
    To push the plot to a gh-pages branch

---
+++ to-gh-pages.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+set -euv
+
+DIR="$(pwd)"
+TARGET="$(dirname "${BASH_SOURCE[0]}")/target"
+
+mkdir -p "$TARGET"
+cd "$TARGET"
+
+
+if false; then
+if [ -d gh-pages ]; then
+  echo "Removing former gh-pages clone"
+  rm -rf gh-pages
+fi
+
+echo "Cloning"
+git clone "https://${GH_TOKEN}@github.com/$REPO.git" -b gh-pages gh-pages
+fi
+cd gh-pages
+
+git config user.name "Travis-CI"
+git config user.email "invalid@travis-ci.com"
+
+cp "$DIR/stats.html" .
+git add -- stats.html
+
+MSG="Update Sonatype statistics"
+
+if git status | grep "nothing to commit" >/dev/null 2>&1; then
+  echo "Nothing changed"
+else
+  git commit -m "$MSG"
+
+  echo "Pushing changes"
+  #git push origin gh-pages
+  echo not
+fi
